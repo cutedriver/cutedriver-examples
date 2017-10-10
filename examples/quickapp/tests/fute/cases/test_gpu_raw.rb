8eb6d94ec31edcd283d060955782d29e7b1d46d7
Juhapekka Piiroinen <juhapekka.piiroinen@link-motion.com>
2017-10-11 00:37:50 +0300

    Fixed issue #15: Added FPS test Fixed issue #17: Added initial CPU test Fixed issue #18: Added initial GPU MEM test Fixed issue #19: Added initial MEM test Fixed issue #20: Added initial PWR test

---
+++ examples/quickapp/tests/fute/cases/test_gpu_raw.rb
@@ -0,0 +1,96 @@
+###########################################################################
+# a GPU MEM test for flickable
+#
+# Author(s):
+#    2017, Juhapekka Piiroinen <juhapekka.piiroinen@link-motion.com>
+#
+# Copyright (c) 2017, Link Motion Oy
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the <organization> nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+###########################################################################
+require "#{File.dirname(__FILE__)}/../shared/helper.rb"
+require "tdriver"
+
+class TestGPURaw < Minitest::Test
+  def setup
+    # Setup cuTeDriver and settings identifiers
+    @sut = TDriver.sut(ENV['QTTASSERVER_SUT'] || 'sut_qt')
+    @sut.set_event_type(:Touch)
+    @app = @sut.run(:name => 'quickapp',
+                    :arguments => '-testability')
+    minitest_verify_true(1,'MainWindow is found.') {
+        @app.child('appWindow')['visible'] == 'true'
+    }
+  end
+
+  def test_flickable_flick
+    appWindow = nil
+
+    # find a appWindow
+    minitest_verify_true(5, "ApplicationWindow can be found") {
+        appWindow = @app.child(:objectName => 'appWindow')
+        appWindow[:objectType] == "Window"
+    }
+
+    # then find the flickable
+    flickable = nil
+    minitest_verify_true(1, "Flickable can be found from appWindow") {
+        flickable = appWindow.child(objectName: "flickable")
+        flickable != nil
+    }
+
+    # Start GPU monitoring
+    @app.log_gpu_mem(:interval => 1, :filePath => '/tmp')  
+
+    # flick flickable
+    minitest_verify_equal(0) {
+        flickable.attribute('contentY').to_i
+    }
+    # flick down with left mouse
+    minitest_verify() {
+        flickable.flick(:Down, :Left)
+    }
+    minitest_verify_equal(0) {
+        flickable.attribute('contentY').to_i
+    }
+
+    # then flick up with left mouse button which scrolls the flickable
+    minitest_verify() {
+        flickable.flick(:Up, :Left)
+    }
+    minitest_verify_greater(0) {
+        flickable.attribute('contentY').to_i
+    }
+
+    # Collect and process GPU data
+    rawData = @app.stop_gpu_log()
+    assert(rawData.length > 0, "raw data was received")
+  end
+
+  def teardown
+    if @app != nil
+        @app.kill
+    end
+  end
+end
---
+++ examples/quickapp/tests/fute/cases/test_pwr.rb
@@ -0,0 +1,112 @@
+###########################################################################
+# a FPS test for flickable
+#
+# Author(s):
+#    2017, Juhapekka Piiroinen <juhapekka.piiroinen@link-motion.com>
+#
+# Copyright (c) 2017, Link Motion Oy
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the <organization> nor the
+#      names of its contributors may be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+###########################################################################
+require "#{File.dirname(__FILE__)}/../shared/helper.rb"
+require "tdriver"
+
+class TestPwr < Minitest::Test
+  def setup
+    # Setup cuTeDriver and settings identifiers
+    @sut = TDriver.sut(ENV['QTTASSERVER_SUT'] || 'sut_qt')
+    @sut.set_event_type(:Touch)
+    @app = @sut.run(:name => 'quickapp',
+                    :arguments => '-testability')
+    minitest_verify_true(1,'MainWindow is found.') {
+        @app.child('appWindow')['visible'] == 'true'
+    }
+  end
+
+  def test_flickable_flick
+    appWindow = nil
+
+    # find a appWindow
+    minitest_verify_true(5, "ApplicationWindow can be found") {
+        appWindow = @app.child(:objectName => 'appWindow')
+        appWindow[:objectType] == "Window"
+    }
+
+    # then find the flickable
+    flickable = nil
+    minitest_verify_true(1, "Flickable can be found from appWindow") {
+        flickable = appWindow.child(objectName: "flickable")
+        flickable != nil
+    }
+
+    # Start PWR monitoring
+    @app.start_pwr_measurement()  
+
+    # flick flickable
+    minitest_verify_equal(0) {
+        flickable.attribute('contentY').to_i
+    }
+
+    # flick down with left mouse
+    minitest_verify() {
+        flickable.flick(:Down, :Left)
+    }
+    minitest_verify_equal(0) {
+        flickable.attribute('contentY').to_i
+    }
+
+    # then flick up with left mouse button which scrolls the flickable
+    minitest_verify() {
+        flickable.flick(:Up, :Left)
+    }
+    minitest_verify_greater(0) {
+        flickable.attribute('contentY').to_i
+    }
+
+    # Collect and process PWR data
+    maximum_pwr = 12.0
+    @app.stop_pwr_measurement().each do |data|
+        value = data[:value].to_f
+        voltage = data[:voltage].to_f
+        current = data[:current].to_f
+        timestamp = data[:time_stamp].to_i
+
+        assert(value == current, "value #{value} should match current #{current}")
+        assert(timestamp > 0, "timestamp should be bigger than zero")
+        if value == -1.0
+            skip("Platform is missing PWR support.")
+        end
+        assert(value >= 0, "value #{value} should be more or equal to zero.")
+        assert(voltage >= 0, "voltage #{voltage} should be more or equal to zero.")
+        assert(current >= 0, "current #{current} should be more or equal to zero.")
+        assert(value < maximum_pwr, "The PWR should be over #{maximum_pwr}. (It was #{value} @ #{timestamp} )")
+    end
+  end
+
+  def teardown
+    if @app != nil
+        @app.kill
+    end
+  end
+end
